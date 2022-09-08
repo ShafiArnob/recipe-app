282712cb218901146d8217cdaa2c7351bb4a2e2c
ShafiArnob <shafiarnob@gmail.com>
2022-09-08 22:15:38 +0600

    fetch data & list the recipe

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,18 @@
-import {useFetch} from 
+import RecipeList from '../../components/RecipeList';
+import {useFetch} from '../../hooks/useFetch'
+import './Home.css'
+
+
 const Home = () => {
+  const {data, isPending, error} = useFetch("http://localhost:3000/recipes")
+
+  console.log(data);
   return (
-    <div>Home</div>
+    <div className='home'>
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>Loading . . . </p>}
+      {data && <RecipeList recipes={data}></RecipeList>}
+    </div>
   )
 }
 
