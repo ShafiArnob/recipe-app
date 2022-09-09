815f1fc320505b403a612fd1b23c809fa00c0ae0
ShafiArnob <shafiarnob@gmail.com>
2022-09-09 10:23:20 +0600

    context api & reducers

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,17 @@
-import './Navbar.css'
 import {Link} from 'react-router-dom'
+
+//components
 import Searchbar from './Searchbar'
+
+//styles
+import './Navbar.css'
+import { useTheme } from '../hooks/useTheme'
+
 const Navbar = () => {
+  const {color, changeColor} = useTheme()
   return (
-    <div className='navbar'>
-      <nav>
+    <div className='navbar' style={{background:color}}>
+      <nav onClick={()=>changeColor('pink')}>
         <Link to='/' className='brand'>
           <h1>Cooking Ninja</h1>
         </Link>
